4edc0d3249267c59da9dcc370bf3967bf5e5045c
unknown <mariyachunawala7@gmail.com>
2022-09-14 13:39:08 +0530

    Add new Requirements

--- requirement.js
+++ requirement.js
@@ -3,14 +3,32 @@
 // 1. get all colleges [Done]
 // 2. get specific college [Done]
 // 3. get specific colleges based on department [Done]
-
+// 
 // POST API:
 // 1. Add new college [Done]
-
-// PUT APIs:
+// 
+// PUT and PATCH APIs:
 // 1. update college name [Done]
 // 2. Update or add new department [Done] 
-
+// 
 // DELETE APIs:
 // 1. Delete a college [Done]
 // 2. Delete a department from a college [Done]
+
+// Department
+// GET APIs:
+// 1. get all Department
+// 2. get specific department
+// 3. get specific department based on college
+// 4. get specific department based on course
+// 
+// POST APIs:
+// 1. Add new department
+// 
+// PUT APIs :
+// 1. update Department name
+// 2. update or add new course
+// 
+// DELETE APIs:
+// 1. Delete a department
+// 2. Delete a course from department
